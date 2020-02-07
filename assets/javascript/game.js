a13ed92f6ca865af32e2d65820586e8d27e0071c
Hung Le <hungle913@gmail.com>
2020-02-07 09:40:16 -0800

    created variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var wins = 0;
+var losses = 0;
+var remaining = 0;
+var guessed = "none"
+var answer = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
