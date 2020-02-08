765b77a0769e251a7b0651daea40ace839d3f393
Hung Le <hungle913@gmail.com>
2020-02-07 21:29:17 -0800

    updated variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,14 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var wins = 0;
 var losses = 0;
-var remaining = 0;
-var guessed = "none"
-var answer = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var guesses = 9;
 
+var winner = document.getElementById("wins");
+var loser = document.getElementById("losses");
+var guessesLeft = document.getElementById("guesses");
+var userGuesses = document.getElementById("userGuesses");
+
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+console.log(computerChoice);
+
+var userChoice = [];
